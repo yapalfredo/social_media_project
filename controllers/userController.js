8af327696dc512b68cd6cb197ae09a71654170f5
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-11 19:32:22 -0400

    installed validation --- 7:32pm

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,8 +11,17 @@ exports.logout = function(){
 }
 
 exports.signup = function(req, res){
+    //creates a new User object, which then passess the req object
+    //as an argument
    let user = new User(req.body)
+   //this will call the singup() protype function of the new User object
    user.signup()
+   //checks if there are any validations errors (array)
+   if (user.errors.length > 0) {
+        res.send(user.errors)
+   } else {
+        res.send("No errors found")
+   }
 }
 
 exports.home = function(req, res){
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 //model is where we enforce the business logic
 
+//npm install validator
+//this will make it easier to validate User input (e.g. email address)
+const validator = require("validator")
 
 //Constructor
 let User = function(data){
@@ -8,12 +11,44 @@ let User = function(data){
     // the appropriate object (in this case User) will be
     // created
    this.data = data
+   this.errors = []
+}
+
+// User input validation logic
+User.prototype.validate = function(){
+    if (this.data.username == "" ) {
+        this.errors.push("You must provide a username.")
+    }
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {
+        this.errors.push("Username can only contains letters and numbers.")
+    }
+    if (!validator.isEmail(this.data.email)) {
+        this.errors.push("You must provide a valid email address.")
+    }
+    if (this.data.password == "" ) {
+        this.errors.push("You must provide a password.")
+    }
+    if (this.data.password.length > 0 && this.data.password.length < 12) {
+        this.errors.push("Password must be atleast 12 characters")
+    }
+    if (this.data.password.length > 100) {
+        this.errors.push("Password cannot exceed 100 characters")
+    }
+    if (this.data.username.length > 0 && this.data.username.length < 8) {
+        this.errors.push("Username must be atleast 8 characters")
+    }
+    if (this.data.username.length > 30) {
+        this.errors.push("Username cannot exceed 30 characters")
+    }
 }
 
 //prototype makes sure that this function() this not get
 //duplicated everytime a new User() model object is called.
 User.prototype.signup = function(){
-
+    //1st step - validate user input
+    this.validate()
+    //2nd step - only if there are no input errors
+    //then save the user input to the database
 }
 
 module.exports = User
