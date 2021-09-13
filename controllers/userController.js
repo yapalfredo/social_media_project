d997347594c72da4c875d084115892a8bccd7234
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-12 23:27:10 -0400

    improved registration form by implementing async / promise / await  ---- @ 11:27pm

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,61 +2,65 @@
 
 // const { resolveInclude } = require('ejs')
 // const { render } = require('../app')
-const User = require("../models/User");
+const User = require("../models/User")
 
 exports.signup = function (req, res) {
   //creates a new User object, which then passess the req object
   //as an argument
-  let user = new User(req.body);
+  let user = new User(req.body)
   //this will call the singup() protype function of the new User object
-  user.signup();
-  //checks if there are any validations errors (array)
-  if (user.errors.length > 0) {
-    user.errors.forEach(function (e) {
-      req.flash("signUpErrors", e);
+  user
+    .signup()
+    .then(() => {
+      req.session.user = { username: user.data.username }
       req.session.save(function () {
-        res.redirect("/");
-      });
-    });
-  } else {
-    res.send("No errors found");
-  }
-};
+        res.redirect("/")
+      })
+    }) //catches any validations errors (array)
+    .catch((signUpErrors) => {
+      signUpErrors.forEach(function (e) {
+        req.flash("signUpErrors", e)
+      })
+      req.session.save(function () {
+        res.redirect("/")
+      })
+    })
+}
 
 //this controls the Login
 exports.login = function (req, res) {
-  let user = new User(req.body);
+  let user = new User(req.body)
 
   //Normall, for using "Promise", you need to use ".then()" and ".catch()"
   user
     .login()
     .then(function (result) {
       //this will create the session
-      req.session.user = { favColor: "blue", username: user.data.username };
+      req.session.user = { favColor: "blue", username: user.data.username }
       //this will trigger manually to save the session in db, then call back function when done saving
       //and redirect to appropriate page
       req.session.save(function () {
-        res.redirect("/");
-      });
+        res.redirect("/")
+      })
     })
     .catch(function (e) {
       //req.session.flash.errors = e
-      req.flash("errors", e);
+      req.flash("errors", e)
       //manually tells session to save the data to db
       req.session.save(function () {
         // res.redirect('/')
-        res.redirect("/");
-      });
-    });
-};
+        res.redirect("/")
+      })
+    })
+}
 
 exports.logout = function (req, res) {
   //will delete the session
   //then redirect to home
   req.session.destroy(function () {
-    res.redirect("/");
-  });
-};
+    res.redirect("/")
+  })
+}
 
 exports.home = function (req, res) {
   //this checks if session is present
@@ -64,7 +68,7 @@ exports.home = function (req, res) {
     //first argument is the name of the template.
     //second argument is the object you want to pass when logged in
     //the username will be accessible from the template 'home-dashboard'
-    res.render("home-dashboard", { username: req.session.user.username });
+    res.render("home-dashboard", { username: req.session.user.username })
   } else {
     //this will make the error message available (from home-guest template) using flash, then deletes it after
     //getting called.
@@ -72,6 +76,6 @@ exports.home = function (req, res) {
     res.render("home-guest", {
       errors: req.flash("errors"),
       signUpErrors: req.flash("signUpErrors"),
-    });
+    })
   }
-};
+}
--- models/User.js
+++ models/User.js
@@ -2,16 +2,16 @@
 
 //npm install bcryptjs
 //For hashing
-const bcryptjs = require("bcryptjs");
+const bcryptjs = require("bcryptjs")
 
 //import the db.js
 //selecting the collection 'users' from mongodb
 //the '.db()' will initialize a database instance
-const usersCollection = require("../db").db().collection("users");
+const usersCollection = require("../db").db().collection("users")
 
 //npm install validator
 //this will make it easier to validate User input (e.g. email address)
-const validator = require("validator");
+const validator = require("validator")
 
 //Constructor
 let User = function (data) {
@@ -19,124 +19,134 @@ let User = function (data) {
   // that way everytime a new constructor is called,
   // the appropriate object (in this case User) will be
   // created
-  this.data = data;
-  this.errors = [];
-};
+  this.data = data
+  this.errors = []
+}
 
 User.prototype.cleanUp = function () {
   // Get rid of any other fake properties
 
   if (typeof this.data.username != "string") {
-    this.data.username = "";
+    this.data.username = ""
   }
   if (typeof this.data.email != "string") {
-    this.data.email = "";
+    this.data.email = ""
   }
   if (typeof this.data.password != "string") {
-    this.data.password = "";
+    this.data.password = ""
   }
   this.data = {
     username: this.data.username.trim().toLowerCase(),
     email: this.data.email.trim().toLowerCase(),
     password: this.data.password,
-  };
-};
+  }
+}
 
 // User input validation logic
-User.prototype.validate = async function () {
-  // if (this.data.username == "" && this.data.email == "" && this.data.password == "" ) {
-  //     this.errors.push("You must provide an username.")
-  //     this.errors.push("You must provide an email")
-  //     this.errors.push("You must provide a password.")
-  // }
-  if (this.data.username == "") {
-    this.errors.push("You must provide an username.");
-  }
-
-  if (this.data.email == "") {
-    this.errors.push("You must provide an email");
-  }
-
-  if (this.data.password == "") {
-    this.errors.push("You must provide a password.");
-  }
-
-  if (this.data.email != "" && !validator.isEmail(this.data.email)) {
-    this.errors.push("You must provide a valid email address.");
-  }
-
-  if (
-    this.data.username != "" &&
-    !validator.isAlphanumeric(this.data.username)
-  ) {
-    this.errors.push("Username can only contains letters and numbers.");
-  }
-
-  if (this.data.password.length > 0 && this.data.password.length < 12) {
-    this.errors.push("Password must be atleast 12 characters");
-  }
-
-  //bcryptjs has a maximum password length of 50
-  if (this.data.password.length > 50) {
-    this.errors.push("Password cannot exceed 50 characters");
-  }
-
-  if (this.data.username.length > 0 && this.data.username.length < 8) {
-    this.errors.push("Username must be atleast 8 characters");
-  }
-
-  if (this.data.username.length > 30) {
-    this.errors.push("Username cannot exceed 30 characters");
-  }
-
-  // Only if username is valid then check if it's already taken
-  if (
-    this.data.username.length > 2 &&
-    this.data.username.length < 31 &&
-    validator.isAlphanumeric(this.data.username)
-  ) {
-    let usernameExists = await usersCollection.findOne({
-      username: this.data.username,
-    });
-    if (usernameExists) {
-      this.errors.push("The username is taken already");
-    }
-  }
-
-  // Only if email is valid then check if it's already taken
-  if (validator.isEmail(this.data.email)) {
-    let emailExists = await usersCollection.findOne({
-      email: this.data.email,
-    });
-    if (emailExists) {
-      this.errors.push("The email address is taken already");
-    }
-  }
-};
+//converted to async (with Promise)
+User.prototype.validate = function(){
+    return new Promise(async (resolve, reject) => {
+        // if (this.data.username == "" && this.data.email == "" && this.data.password == "" ) {
+        //     this.errors.push("You must provide an username.")
+        //     this.errors.push("You must provide an email")
+        //     this.errors.push("You must provide a password.")
+        // }
+        if (this.data.username == "") {
+          this.errors.push("You must provide an username.")
+        }
+      
+        if (this.data.email == "") {
+          this.errors.push("You must provide an email")
+        }
+      
+        if (this.data.password == "") {
+          this.errors.push("You must provide a password.")
+        }
+      
+        if (this.data.email != "" && !validator.isEmail(this.data.email)) {
+          this.errors.push("You must provide a valid email address.")
+        }
+      
+        if (
+          this.data.username != "" &&
+          !validator.isAlphanumeric(this.data.username)
+        ) {
+          this.errors.push("Username can only contains letters and numbers.")
+        }
+      
+        if (this.data.password.length > 0 && this.data.password.length < 12) {
+          this.errors.push("Password must be atleast 12 characters")
+        }
+      
+        //bcryptjs has a maximum password length of 50
+        if (this.data.password.length > 50) {
+          this.errors.push("Password cannot exceed 50 characters")
+        }
+      
+        if (this.data.username.length > 0 && this.data.username.length < 8) {
+          this.errors.push("Username must be atleast 8 characters")
+        }
+      
+        if (this.data.username.length > 30) {
+          this.errors.push("Username cannot exceed 30 characters")
+        }
+      
+        // Only if username is valid then check if it's already taken
+        if (
+          this.data.username.length > 2 &&
+          this.data.username.length < 31 &&
+          validator.isAlphanumeric(this.data.username)
+        ) {
+          let usernameExists = await usersCollection.findOne({
+            username: this.data.username,
+          })
+          if (usernameExists) {
+            this.errors.push("The username is taken already")
+          }
+        }
+      
+        // Only if email is valid then check if it's already taken
+        if (validator.isEmail(this.data.email)) {
+          let emailExists = await usersCollection.findOne({
+            email: this.data.email,
+          })
+          if (emailExists) {
+            this.errors.push("The email address is taken already")
+          }
+        }
+        resolve()
+      })
+}
 
 //prototype makes sure that this function() this not get
 //duplicated everytime a new User() model object is called.
-User.prototype.signup = function () {
-  //1st step - cleanUp and validate user input
-  this.cleanUp();
-  this.validate();
-
-  //2nd step - only if there are no input errors
-  //then save the user input to the database
-  if (!this.errors.length) {
-    //hash user password
-    let salt = bcryptjs.genSaltSync(10);
-    this.data.password = bcryptjs.hashSync(this.data.password, salt);
-    usersCollection.insertOne(this.data);
-  }
-};
+User.prototype.signup = function(){
+    return new Promise(async (resolve, reject) => {
+        //1st step - cleanUp and validate user input
+        this.cleanUp()
+        await this.validate()
+      
+        //2nd step - only if there are no input errors
+        //then save the user input to the database
+        if (!this.errors.length) {
+          //hash user password
+          let salt = bcryptjs.genSaltSync(10)
+          this.data.password = bcryptjs.hashSync(this.data.password, salt)
+          await usersCollection.insertOne(this.data)
+          resolve()
+        }
+        else{
+            reject(this.errors)
+        }
+      })
+}
 
 //User Login Model
 User.prototype.login = function () {
   //Promise is the latest standard for asynchronus processing
   //arrow function doesn't manipulate the 'this' keyword
   return new Promise((resolve, reject) => {
-    this.cleanUp();
+    this.cleanUp()
 
     //CRUD (READ PART)
     //Converted to PROMISE
@@ -151,15 +161,15 @@ User.prototype.login = function () {
           requestedUser &&
           bcryptjs.compareSync(this.data.password, requestedUser.password)
         ) {
-          resolve(true);
+          resolve(true)
         } else {
-          reject("Incorrect Username / Password !");
+          reject("Incorrect Username / Password !")
         }
       })
       .catch(function () {
-        reject("There was an error during login ");
-      });
-  });
-};
+        reject("There was an error during login ")
+      })
+  })
+}
 
-module.exports = User;
+module.exports = User
