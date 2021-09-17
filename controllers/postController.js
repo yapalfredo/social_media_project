7bc589c94a0c5b1d2233c8d0615d97d35424967a
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-17 16:44:44 -0400

    modified postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -31,17 +31,29 @@ exports.viewSingle = async function(req, res){
 }
 
 exports.viewEditScreen = async function(req, res){
-    try{
-        let post = await Post.findSingleById(req.params.id)
-        if (post.authorId == req.visitorId) {
-            res.render("edit-post", {post: post})
+    // try{
+    //     let post = await Post.findSingleById(req.params.id)
+    //     if (post.authorId == req.visitorId) {
+    //         res.render("edit-post", {post: post})
+    //     } else {
+    //         req.flash("errors", "You do not have permission to perform that action")
+    //         req.session.save(()=> res.redirect("/"))
+    //     }
+    // }catch {
+    //     res.render("404")
+    // }
+    try {
+        let post = await Post.findSingleById(req.params.id, req.visitorId)
+        if (post.isVisitorOwner) {
+          res.render("edit-post", {post: post})
         } else {
-            req.flash("errors", "You do not have permission to perform that action")
-            req.session.save(()=> res.redirect("/"))
+          req.flash("errors", "You do not have permission to perform that action.")
+          req.session.save(() => res.redirect("/"))
         }
-    }catch {
+      } catch {
         res.render("404")
-    }
+      }
+    
 }
 
 exports.edit = function(req, res){
