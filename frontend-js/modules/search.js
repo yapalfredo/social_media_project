1e625f0527976515814a0a5cf71411734eb6bddb
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-18 19:11:52 -0400

    fully implemented search

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -69,24 +69,16 @@ export default class Search {
         if (posts.length) {
             this.resultsArea.innerHTML = `
             <div class="list-group shadow-sm">
-            <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
+            <div class="list-group-item active"><strong>Search Results</strong> (${posts.length > 1 ? `${posts.length} items found` : `1 item found`})</div>
+            ${posts.map(post => {
+                let postDate = new Date(post.createdDate)
+                return `
+                <a href="/post/${post._id}" class="list-group-item list-group-item-action">
+                <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${post.title}</strong>
+                <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth()}/${postDate.getDate()}/${postDate.getFullYear()}</span>
+                </a>
+              `
+            }).join('')}
           </div>
           `
         } else {
