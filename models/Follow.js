f1f4ad8419e2e74c5f7df7433614c330f8ed8e13
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-19 17:56:28 -0400

    follows are saving to the db 5:56pm

--- models/Follow.js
+++ models/Follow.js
@@ -1,5 +1,39 @@
-let Follow = function(){
+const usersCollection = require('../db').db().collection("users")
+const followsCollection = require('../db').db().collection("follows")
+const ObjectID = require('mongodb').ObjectId
 
+let Follow = function(followedUsername, authorId){
+    this.followedUsername = followedUsername
+    this.authorId = authorId
+    this.errors = []
+}
+
+Follow.prototype.cleanUp = function(){
+    if (typeof(this.followedUsername) != "string") {this.followedUsername = ""}
+}
+
+Follow.prototype.validate = async function(){
+    //Followed Username must exist in the database
+    let followedAccount = await usersCollection.findOne({username: this.followedUsername})
+    if (followedAccount) {
+        this.followedId = followedAccount._id
+    } else {
+        this.errors.push("You cannot follow a user which doesn't exist")
+    }
+}
+
+Follow.prototype.create = function(){
+    return new Promise(async (resolve, reject) => {
+        this.cleanUp()
+        await this.validate()
+
+        if (this.errors.length) {
+            reject(this.errors)
+        }else {
+            await followsCollection.insertOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+            resolve()
+        }
+    })
 }
 
 module.exports = Follow
