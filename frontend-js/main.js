99a46cbdc3eb22ddfab48ad97280ff6bf4189c88
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-20 16:38:01 -0400

    added chat template ---@ 4:37pm

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,3 +1,5 @@
 import Search from "./modules/search"
+import Chat from "./modules/chat"
 
 if (document.querySelector(".header-search-icon")) {new Search()}
+if (document.querySelector("#chat-wrapper")){ new Chat()}
