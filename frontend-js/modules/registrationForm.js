894a9435cc3a7b2a295bf2f6073c24c8ba31743a
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-20 22:37:45 -0400

    still working on live form validation ----@10:37pm

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -24,13 +24,49 @@ export default class RegistrationForm {
     }
 
     usernameHandler(){
-        alert("User Name Handler Method")
+        this.username.errors = false
+        this.usernameImmediately()
+        clearTimeout(this.username.timer)
+        this.username.timer = setTimeout(() => this.usernameAfterDelay(), 2000)
+    }
+
+    usernameImmediately(){
+        //regex only accepts alphanumeric
+        if (this.username.value != "" && !/^([a-zA-Z0-9]+)$/.test(this.username.value)) {
+            this.showValidationError(this.username, "Only alphanumeric is allowed for username")
+        } 
+
+        if (!this.username.errors){
+         this.hideValidationError(this.username)   
+        }
+
+        if (this.username.value.length > 30){
+            this.showValidationError(this.username, "You can only have 30 characters for username")
+        }
+
+    }
+
+    hideValidationError(element){
+        element.nextElementSibling.classList.remove("liveValidateMessage--visible")
+    }
+
+    showValidationError(element, message){
+        element.nextElementSibling.innerHTML = message
+        element.nextElementSibling.classList.add("liveValidateMessage--visible")
+        element.errors = true
+
+    }
+
+    usernameAfterDelay(){
+        if (this.username.value.length < 3) {
+            this.showValidationError(this.username, "You must have at least 3 characters for username")
+        }
     }
 
     insertValidationElements(){
         this.allFields.forEach(function(element){
             element.insertAdjacentHTML("afterend", `
-            <div class="alert alert-danger small liveValidateMessage">Hello</div>
+            <div class="alert alert-danger small liveValidateMessage"></div>
             `)
         })
     }
